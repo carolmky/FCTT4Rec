6f8fa178192878ec6a9430156a3e52e15b814e5b
马凯洋 <53643642+carolmky@users.noreply.github.com>
2022-06-15 22:26:10 +0800

    Add files via upload

---
+++ fctt/trainers/bert.py
@@ -0,0 +1,42 @@
+from .base import AbstractTrainer
+from .utils import recalls_and_ndcgs_for_ks
+
+
+class BERTTrainer(AbstractTrainer):
+    def __init__(self, args, model, train_loader, val_loader, test_loader, export_root):
+        super().__init__(args, model, train_loader, val_loader, test_loader, export_root)
+
+    @classmethod
+    def code(cls):
+        return 'bert'
+
+    def add_extra_loggers(self):
+        pass
+
+    def log_extra_train_info(self, log_data):
+        pass
+
+    def calculate_loss(self, batch,batch_idx,is_gcn):
+        # loss = self.model(batch, loss=True)
+        # loss = loss.mean()
+        # return loss
+        d = self.model(batch,batch_idx,is_gcn)
+        loss, loss_cnt = d['loss'], d['loss_cnt']
+        loss = (loss * loss_cnt).sum() / loss_cnt.sum()
+        return loss
+
+    def calculate_metrics(self, batch,batch_idx,is_gcn):
+        labels = batch['labels']
+        scores = self.model(batch,batch_idx,is_gcn)['scores']  # B x C
+        # scores = scores.gather(1, candidates)  # B x C
+
+        metrics = recalls_and_ndcgs_for_ks(scores, labels, self.metric_ks)
+        return metrics
+
+    def gcn(self,batch,batch_idx,is_gcn):
+
+        emb_user,emb_item = self.model(batch,batch_idx,is_gcn)
+        self.is_gcn=False
+        return emb_user,emb_item
+
+
