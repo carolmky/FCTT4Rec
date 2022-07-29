aa6afa54d2fffb2ca6821415ff3ca49f679e01e1
马凯洋 <53643642+carolmky@users.noreply.github.com>
2022-07-29 22:50:44 +0800

    Add files via upload

---
+++ FCTT/datasets/__init__.py
@@ -0,0 +1,13 @@
+from .base import AbstractDataset
+from FCTT.utils import all_subclasses
+from FCTT.utils import import_all_subclasses
+import_all_subclasses(__file__, __name__, AbstractDataset)
+
+DATASETS = {c.code():c
+            for c in all_subclasses(AbstractDataset)
+            if c.code() is not None}
+
+
+def dataset_factory(args):
+    dataset = DATASETS[args.dataset_code]
+    return dataset(args)
